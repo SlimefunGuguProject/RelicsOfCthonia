c2028eda43c80da40a52b1ad9f8aa49f0f7fe62c
FN-FAL113 <orbetajeff@gmail.com>
2022-09-16 10:28:19 +0800

    Respect disabled relics globally or per world

--- src/main/java/ne/fnfal113/relicsofcthonia/listeners/MobKillListener.java
+++ src/main/java/ne/fnfal113/relicsofcthonia/listeners/MobKillListener.java
@@ -4,11 +4,14 @@ import lombok.Getter;
 import ne.fnfal113.relicsofcthonia.RelicsOfCthonia;
 import ne.fnfal113.relicsofcthonia.relics.abstracts.AbstractRelic;
 import ne.fnfal113.relicsofcthonia.utils.Utils;
+import org.bukkit.World;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
 
 import java.util.List;
 import java.util.Map;
@@ -21,30 +24,61 @@ public class MobKillListener implements Listener {
     private final Map<AbstractRelic, List<String>> whereToDropMobMap = RelicsOfCthonia.getInstance().getRelicsRegistry().getWhereToDropMobMap();
 
     @EventHandler
-    public void onMobKill(EntityDeathEvent event) {
-        LivingEntity livingEntity = event.getEntity();
-
-        if(!livingEntity.getWorld().getName().equals("world_nether")){
+    public void onMobSpawn(CreatureSpawnEvent event){
+        if(event.isCancelled()){
             return;
         }
+
+        if(event.getSpawnReason() != CreatureSpawnEvent.SpawnReason.SPAWNER){
+            return;
+        }
+
+        if(event.getEntity().getWorld().getEnvironment() != World.Environment.NETHER){
+            return;
+        }
+
+        LivingEntity entity = event.getEntity();
+
+        entity.setMetadata("relic_spawned_mob", new FixedMetadataValue(RelicsOfCthonia.getInstance(), "spawned_mob"));
+    }
+
+    @EventHandler
+    public void onMobKill(EntityDeathEvent event) {
+        LivingEntity livingEntity = event.getEntity();
 
         if(livingEntity.getKiller() == null){
             return;
         }
 
-        String entityType = livingEntity.getType().name().toLowerCase();
+        if(livingEntity.getWorld().getEnvironment() != World.Environment.NETHER){
+            return;
+        }
 
+        if(livingEntity.hasMetadata("relic_spawned_mob")){
+            livingEntity.removeMetadata("relic_spawned_mob", RelicsOfCthonia.getInstance());
+
+            return;
+        }
+
+        String entityType = livingEntity.getType().name().toLowerCase();
+        World world = livingEntity.getWorld();
         ThreadLocalRandom currentRandomThread = ThreadLocalRandom.current();
         AtomicInteger i = new AtomicInteger(0);
 
         Utils.createAsyncTask(asyncTask -> {
             for (Map.Entry<AbstractRelic, List<String>> entry: getWhereToDropMobMap().entrySet()){
+                AbstractRelic abstractRelic = entry.getKey();
+
+                if(abstractRelic.isDisabledIn(world) || abstractRelic.isDisabled()){
+                    continue;
+                }
+
                 if(entry.getValue().contains(entityType)){
                     double randomOrigin = currentRandomThread.nextDouble(0.0, 60);
                     double randomNum = currentRandomThread.nextDouble(randomOrigin, 100);
 
-                    if(randomNum < entry.getKey().getDropChance()) {
-                        ItemStack drop = entry.getKey().setRelicCondition(true, 0);
+                    if(randomNum < abstractRelic.getDropChance()) {
+                        ItemStack drop = abstractRelic.setRelicCondition(true, 0);
                         Utils.createSyncTask(syncTask -> livingEntity.getWorld().dropItemNaturally(livingEntity.getLocation(), drop));
 
                         i.getAndIncrement();
